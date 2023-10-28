469b6fc09bf1db2724de5b5885618b79d538d9d1
HP <HP@DESKTOP-B1KMEQH>
2023-10-28 12:28:51 +0530

    added logo and name in tab section and added image

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,23 @@
+
+function Home() {
+    return (
+        <>
+            <div className="custom-footer-bg p-3 mt-2">
+            asdfasdf
+              
+            </div>
+            <div className="custom-footer-bg1 p-3 mt-2">
+                <img src="/finalfs.png" alt="" className="compressed-image" />
+
+            </div>
+            <div className="custom-footer-bg2 p-3 mt-2">
+                asdfasdf
+
+            </div>
+            
+        </>
+       
+    );
+}
+
+export default Home;
